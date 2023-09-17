30b563be694e9007adf9e92da22c1db2fd45b53f
laibaijiang <your.email@example.com>
2023-09-17 20:11:39 +0800

    数据字典相关开发完毕

---
+++ backend/service/service-cmn/src/main/java/com/laibaijiang/yygh/cmn/mapper/DictMapper.java
@@ -0,0 +1,8 @@
+package com.laibaijiang.yygh.cmn.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lbj.yygh.model.cmn.Dict;
+import org.apache.ibatis.annotations.Mapper;
+
+public interface DictMapper extends BaseMapper<Dict> {
+}
---
+++ backend/service/service-cmn/src/main/java/com/laibaijiang/yygh/cmn/service/impl/DictServiceImpl.java
@@ -0,0 +1,77 @@
+package com.laibaijiang.yygh.cmn.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.alibaba.excel.EasyExcel;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.laibaijiang.yygh.cmn.listener.DictListener;
+import com.laibaijiang.yygh.cmn.mapper.DictMapper;
+import com.laibaijiang.yygh.cmn.service.DictService;
+import com.lbj.yygh.model.cmn.Dict;
+import com.lbj.yygh.vo.cmn.DictEeVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
+
+    @Override
+    @Cacheable(value = "dict",keyGenerator = "keyGenerator") // 将最终的dictList放入缓存, key dict报名类名方法名
+    public List<Dict> findChildData(Long id) {
+        // sql select * from cmn where parent_id = id
+        QueryWrapper<Dict> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id", id);
+        List<Dict> dictList = baseMapper.selectList(wrapper);
+        for (Dict dict : dictList) {
+            Long dictId = dict.getId();
+            boolean child = hashChild(dictId);
+            dict.setHasChildren(child);
+        }
+        return dictList;
+    }
+
+    @Override
+    public void exportData(HttpServletResponse response) {
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        String fileName = "dict";
+        response.setHeader("Content-disposition", "attachment;filename="+ fileName + ".xlsx");
+        List<Dict> dictList = baseMapper.selectList(null);
+        ArrayList<DictEeVo> voList = new ArrayList<>();
+        for (Dict dict : dictList) {
+            DictEeVo dictEeVo = new DictEeVo();
+            BeanUtil.copyProperties(dict, dictEeVo);
+            voList.add(dictEeVo);
+        }
+        try {
+            EasyExcel.write(response.getOutputStream(), DictEeVo.class).sheet("数据字典").doWrite(voList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    @CacheEvict(value = "dict", allEntries=true) // 把该规则下的所有缓存数据进行清除
+    public void importData(MultipartFile file) {
+        try {
+            EasyExcel.read(file.getInputStream(),DictEeVo.class, new DictListener(baseMapper)).sheet().doRead();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 判断id下是否还有子数据
+    private boolean hashChild(Long dictId) {
+        QueryWrapper<Dict> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id",dictId);
+        return baseMapper.selectCount(wrapper) > 0;
+    }
+}
